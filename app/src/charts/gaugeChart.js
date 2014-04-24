f7e50f55248bde8bb8733cab2ab53dabaa27804e
elodiebatista <elodie.batista@live.fr>
2014-04-24 22:38:52 +0200

    Forgot this one

--- app/src/charts/gaugeChart.js
+++ app/src/charts/gaugeChart.js
@@ -27,65 +27,72 @@ module.directive('gaugeChart', function(colorService) {
         }];
       };
 
-
-      $(element).highcharts({
-        chart: {
-          type: 'gauge'
-        },
-        title: {
-          text: scope.titleChart,
-          style: {
-            fontVariant: 'small-caps',
-            fontSize: '18px'
-          }
-        },
-        pane: {
-          startAngle: -150,
-          endAngle: 150,
-          background: [{
-            backgroundColor: '#FFF'
-          }]
-        },
-        yAxis: {
-          min: 0,
-          max: 100,
-
-          minorTickInterval: 'auto',
-          minorTickWidth: 1,
-          minorTickLength: 10,
-          minorTickPosition: 'inside',
-          minorTickColor: 'transparent',
-
-          tickPixelInterval: 30,
-          tickWidth: 1,
-          tickPosition: 'inside',
-          tickLength: 10,
-          tickColor: '#000',
-
-          labels: {
-            step: 2,
-            rotation: 'auto'
+      scope.draw = function() {
+        $(element).highcharts({
+          chart: {
+            type: 'gauge'
           },
           title: {
-            text: ''
+            text: scope.titleChart,
+            style: {
+              fontVariant: 'small-caps',
+              fontSize: '18px'
+            }
           },
-          plotBands: [{
-            from: 0,
-            to: 30,
-            color: colorService.green.normal
-          }, {
-            from: 30,
-            to: 70,
-            color: colorService.orange.normal
-          }, {
-            from: 70,
-            to: 100,
-            color: colorService.red.normal
-          }]
-        },
+          pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [
+              {
+                backgroundColor: '#FFF'
+              }
+            ]
+          },
+          yAxis: {
+            min: 0,
+            max: 100,
 
-        series: scope.formatData()
-      });
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: 'transparent',
+
+            tickPixelInterval: 30,
+            tickWidth: 1,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#000',
+
+            labels: {
+              step: 2,
+              rotation: 'auto'
+            },
+            title: {
+              text: ''
+            },
+            plotBands: [
+              {
+                from: 0,
+                to: 30,
+                color: colorService.green.normal
+              },
+              {
+                from: 30,
+                to: 70,
+                color: colorService.orange.normal
+              },
+              {
+                from: 70,
+                to: 100,
+                color: colorService.red.normal
+              }
+            ]
+          },
+
+          series: scope.formatData()
+        });
+      };
     }
   };
 });
